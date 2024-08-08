c1bc0250e1ec1dbd8c300031bca3e8cd6e056f0d
Luiz Augusto <srluizaugusto@outlook.com>
2024-08-08 15:51:58 -0300

    Feat: Criação da Classe ContaTerminal e do metodo para obtenção dos dados inseridos pelo usuário via terminal e exibição de mensagem personalizada com esses dados

---
+++ src/main/java/org/example/ContaTerminal.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+    int numero;
+    String agencia;
+    String nomeCliente;
+    float saldo;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+}
+
+
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ContaTerminal conta = new ContaTerminal();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o número da Conta: ");
+        conta.numero = sc.nextInt();
+
+        sc = new Scanner(System.in); //Limpando a variável pois estava pulando a etapa
+        System.out.println("Digite o número do Agência: ");
+        conta.agencia = sc.nextLine();
+
+        System.out.println("Digite o nome do cliente: ");
+        conta.nomeCliente = sc.nextLine();
+
+        System.out.println("Digite o saldo atual: ");
+        conta.saldo = sc.nextFloat();
+
+
+
+        System.out.println("\n\n________________________________________________\n\n"+
+                "Olá "+ conta.nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é " +
+                conta.agencia+", conta " +conta.numero+ " e seu saldo: R$"+ conta.saldo +
+                " já está disponível para saque");
+    }
+}
